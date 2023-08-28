c317cce1f4f76bf4797715ae2cea0f0ce3029b01
golnaz <golnazgoli916@yahoo.com>
2023-08-28 21:05:31 +0330

    feat(urls): configure URL patterns for project

--- kernel/urls.py
+++ kernel/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-]
+    prefix_default_language=False
+)
+
+urlpatterns += [
+    path("i18n/", include("django.conf.urls.i18n")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
